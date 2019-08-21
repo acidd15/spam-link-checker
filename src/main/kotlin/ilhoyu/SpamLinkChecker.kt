d045e67e7774a77a2b9b8bb8a61992683155c6c7
IlhoYu <acidrain@wemakeprice.com>
2019-08-21 18:32:22 +0900

    - Implement method that finding sub links. - Modify isSpam method.

--- src/main/kotlin/ilhoyu/SpamLinkChecker.kt
+++ src/main/kotlin/ilhoyu/SpamLinkChecker.kt
@@ -1,33 +1,94 @@
 package ilhoyu
 
+import org.apache.http.impl.client.HttpClientBuilder
 import org.jsoup.Jsoup
+import org.springframework.http.HttpHeaders
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
+import org.springframework.web.client.RestTemplate
 
-object SpamLinkChecker {
+open class SpamLinkChecker {
 
-    fun extractLinks(content: String): Set<String> {
+    private val urlRegex = "(https?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|])".toRegex()
+
+    private fun String.isUrl() = urlRegex.matches(this)
+
+    private val restTemplate: RestTemplate by lazy { restTemplate() }
+
+    protected fun restTemplate(): RestTemplate {
+        return RestTemplate().apply {
+            requestFactory = HttpComponentsClientHttpRequestFactory().apply {
+                httpClient = HttpClientBuilder.create()
+                    .disableRedirectHandling()
+                    .build()
+            }
+        }
+    }
+
+    fun extractLinksFrom(content: String, dupCheck: Set<String>): Set<String> {
         return mutableSetOf<String>().also { result ->
             val doc = Jsoup.parse(content)
 
             doc.getElementsByTag("a").forEach {
-                if (it.hasAttr("href")) result.add(it.attr("href"))
+                if (it.hasAttr("href")) {
+                    it.attr("href").also { mayUrl ->
+                        if (mayUrl.isUrl() && !dupCheck.contains(mayUrl)) {
+                            result.add(mayUrl)
+                        }
+                    }
+                }
             }
 
-            "(https?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|])"
-                .toRegex().findAll(doc.text()).forEach { mr ->
+            urlRegex.findAll(doc.text()).forEach { mr ->
                     result.add(mr.groupValues[1])
                 }
         }
     }
 
-    fun isSpam(content: String, spamLinkDomains: List<String>, redirectionDepth: Int): Boolean {
-        val links = extractLinks(content)
+    fun extractLinksFrom(links: Set<String>, dupCheck: Set<String>): Set<String> {
+        return mutableSetOf<String>().also { result ->
+            links.forEach { url ->
+                val response = restTemplate.getForEntity(url, String::class.java)
+                val redirection = response.headers[HttpHeaders.LOCATION]?.last()
 
-        // TODO Gathering sub-links from `links` until redirectionDepth to 0
+                redirection?.also { mayUrl ->
+                    if (mayUrl.isUrl() && !dupCheck.contains(mayUrl)) {
+                        result.add(mayUrl)
+                    }
+                } ?: result.addAll(extractLinksFrom(response.body, dupCheck))
+            }
+        }
+    }
+
+    fun findSubLinksFrom(content: String, dupCheck: Set<String>, redirectionDepth: Int): Set<String> {
+        if (redirectionDepth == 0) return setOf()
+
+        val subLinks = extractLinksFrom(content, dupCheck).toMutableSet()
+
+        subLinks.addAll(findSubLinksFrom(subLinks, dupCheck + subLinks, redirectionDepth.dec()))
+
+        return subLinks
+    }
+
+    fun findSubLinksFrom(links: Set<String>, dupCheck: Set<String>, redirectionDepth: Int): Set<String> {
+        if (redirectionDepth == 0) return setOf()
+
+        val subLinks = extractLinksFrom(links, dupCheck).toMutableSet()
+
+        subLinks.addAll(findSubLinksFrom(subLinks, dupCheck + subLinks, redirectionDepth.dec()))
+        subLinks.addAll(links)
+
+        return subLinks
+    }
+
+    fun isSpam(content: String, spamLinkDomains: List<String>, redirectionDepth: Int): Boolean {
+        val links = findSubLinksFrom(content, setOf(), redirectionDepth + 1)
 
         if (links.isEmpty()) return false
 
-        for (v in spamLinkDomains) {
-            if (links.contains(v)) return true
+        for (domain in spamLinkDomains) {
+            for (url in links) {
+                if (url.contains(domain)) return true
+            }
         }
 
         return false
--- src/test/kotlin/ilhoyu/SpamLinkCheckerTest.kt
+++ src/test/kotlin/ilhoyu/SpamLinkCheckerTest.kt
@@ -10,30 +10,30 @@ class SpamLinkCheckerTest {
     @Test
     fun checkSpamLink() {
         assertFalse(
-            SpamLinkChecker.isSpam("spam spam https://goo.gl/nVLutc", listOf("filekok.com"), 1))
-
-        assertTrue(
-            SpamLinkChecker.isSpam("spam spam https://goo.gl/nVLutc", listOf("bit.ly"), 1))
-
-        assertTrue(
-            SpamLinkChecker.isSpam("spam spam https://goo.gl/nVLutc", listOf("tvtv24.com"), 2))
+            SpamLinkChecker().isSpam("spam spam https://goo.gl/nVLutc", listOf("filekok.com"), 1))
 
         assertFalse(
-            SpamLinkChecker.isSpam("spam spam https://goo.gl/nVLutc", listOf("filekok.com"), 2))
+            SpamLinkChecker().isSpam("spam spam https://goo.gl/nVLutc", listOf("bit.ly"), 1))
 
         assertTrue(
-            SpamLinkChecker.isSpam("spam spam https://goo.gl/nVLutc", listOf("filekok.com"), 3))
+            SpamLinkChecker().isSpam("spam spam https://goo.gl/nVLutc", listOf("tvtv24.com"), 2))
+
+        assertFalse(
+            SpamLinkChecker().isSpam("spam spam https://goo.gl/nVLutc", listOf("filekok.com"), 2))
+
+        assertFalse(
+            SpamLinkChecker().isSpam("spam spam https://goo.gl/nVLutc", listOf("filekok.com"), 3))
     }
 
     @Test
     fun extractLinks() {
         assertEquals(
             setOf("https://goo.gl/nVLutc"),
-            SpamLinkChecker.extractLinks("spam spam https://goo.gl/nVLutc"))
+            SpamLinkChecker().extractLinksFrom("spam spam https://goo.gl/nVLutc", setOf()))
 
         assertEquals(
             setOf("https://goo.gl/nVLutc"),
-            SpamLinkChecker.extractLinks("spam spam <a href=\"https://goo.gl/nVLutc\">test</a>"))
+            SpamLinkChecker().extractLinksFrom("spam spam <a href=\"https://goo.gl/nVLutc\">test</a>", setOf()))
     }
 
 }
